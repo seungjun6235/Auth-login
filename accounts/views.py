b14b5ddfbef3986727256f6a6d81bec483240b32
seungjun6235 <58593578+seungjun6235@users.noreply.github.com>
2023-11-07 11:30:05 +0900

    Signup 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid(): #올바른 입력한 경우: 흔한비밀번호 X
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'signup.html',context)
--- auth/urls.py
+++ auth/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
+    path('accounts/',include('accounts.urls')),
+
 ]
