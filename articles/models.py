1aa601a0cb4de695567e7c57157dac9af6207cd7
seungjun6235 <58593578+seungjun6235@users.noreply.github.com>
2023-11-07 14:36:22 +0900

    Article 모델링 마이그레이션

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User # 방법1
+from django.conf import settings # 방법2
+from django.contrib.auth import get_user_model # 방법3
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    #유저모델을 참조하는경우
+    # 방법1.(권장X)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    #방법2.(권장0) settings.AUTH_USER_MODEL == 'accounts.User'
+
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    #방법3. (권장0)
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
