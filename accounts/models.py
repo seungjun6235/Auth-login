3855b01037b49c707c13a2e49a0243ed3cc350fc
seungjun6235 <58593578+seungjun6235@users.noreply.github.com>
2023-11-08 10:34:43 +0900

    Comment 모델링 마이그레이션

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,5 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+    # aritcle_set = 자동 컬럼추가
+    # comment_set 
--- articles/models.py
+++ articles/models.py
@@ -18,3 +18,16 @@ class Article(models.Model):
 
     #방법3. (권장0)
     user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    
+    # 1:N 에서 1은 _id, N은 _set
+    #comment_set = 장고가 자동으로 추가해주는 컬럼
+    #user_id
+
+class Comment(models.Model):
+    content =  models.TextField()
+    article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
+    #article_id = 장고가 자동으로 컬럼 추가
+    #user_id 
+    
